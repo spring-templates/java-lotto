e8925d50ac0fb3e7170c11053ef7be18b6ec1b84
ohchansol <haxr369@gmail.com>
2024-02-28 16:39:44 +0900

    Create Customer class

---
+++ src/main/java/org/example/actors/Customer.java
@@ -0,0 +1,23 @@
+package org.example.actors;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Customer {
+    private int purchaseAmount;
+    private List<Lotto> lottoList = new ArrayList<>();
+    private long totalWinningAmount;
+
+    public int setPurchaseAmount(){
+        // 당첨금 입력 받아서 저장 및 출력
+        return 0;
+    }
+
+    public float checkLotteryResult(){
+        return 0.0F;
+    }
+
+    public void showLotteryResult(){
+
+    }
+}
--- src/main/java/org/example/actors/Lotto.java
+++ src/main/java/org/example/actors/Lotto.java
@@ -1,8 +1,14 @@
 package org.example.actors;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.example.Winnings;
 
 public class Lotto {
+    @Getter
     private int[] lottoNumbers;
+
+    @Getter
+    @Setter
     private Winnings winnings;
 }
