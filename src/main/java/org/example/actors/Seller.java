3f8d1d8f43f3c800853abb2018064854186136ac
ohchansol <haxr369@gmail.com>
2024-02-28 16:43:02 +0900

    Create Seller class

---
+++ src/main/java/org/example/actors/Seller.java
@@ -0,0 +1,18 @@
+package org.example.actors;
+
+import java.util.List;
+
+public class Seller {
+
+    public boolean validateLotto(Lotto lotto){
+        return true;
+    }
+
+    public List<Lotto> createLottos(int count){
+        return null;
+    }
+
+    public boolean vaildatePurchaseAmount(int amount){
+        return true;
+    }
+}
