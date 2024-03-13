6d18a24a356f2391fec989e2c5d40e156fec31e5
Hyeon-hak Kim <07ily@naver.com>
2024-03-14 03:01:15 +0900

    ✨ Feat : define base class

---
+++ src/main/java/base/Generator.java
@@ -0,0 +1,13 @@
+package base;
+
+public abstract class Generator<IN extends InputSchema, OUT extends OutputSchema> {
+    protected Validator<IN> validator;
+
+
+    public OUT generate(IN in) throws IllegalArgumentException {
+        validator.validate(in);
+        return getEntity(in).toDto();
+    }
+
+    protected abstract Entity<IN, OUT> getEntity(IN in);
+}
