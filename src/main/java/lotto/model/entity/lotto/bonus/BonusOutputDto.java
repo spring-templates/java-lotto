78795d7b0f7f3e946136c3a1e2e41b0dbea9a57f
Hyeon-hak Kim <07ily@naver.com>
2024-03-14 23:20:28 +0900

    ✨ Feat(Bonus) : redefine `BonusDto`
    - `isBonusMatched()`

--- src/main/java/lotto/model/entity/lotto/bonus/BonusOutputDto.java
+++ src/main/java/lotto/model/entity/lotto/bonus/BonusOutputDto.java
@@ -1,10 +1,16 @@
 package lotto.model.entity.lotto.bonus;
 
+import lotto.model.entity.lotto.ILottoOutputDto;
+
 public record BonusOutputDto(
         Integer bonusNumber
 ) implements IBonusOutputDto {
     @Override
     public String toString() {
         return bonusNumber.toString();
+    }
+
+    public Boolean isBonusMatched(ILottoOutputDto bonus) {
+        return bonus.numbers().contains(bonusNumber);
     }
 }
