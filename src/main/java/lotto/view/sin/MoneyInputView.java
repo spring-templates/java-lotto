38c1e99cfea4a38f970f2523ce1299d33a024562
Hyeon-hak Kim <07ily@naver.com>
2024-03-18 15:07:42 +0900

    ✨ Feat(view) : 재구현
    Signed-off-by: Hyeon-hak Kim <07ily@naver.com>

--- src/main/java/lotto/view/sin/MoneyInputView.java
+++ src/main/java/lotto/view/sin/MoneyInputView.java
@@ -1,12 +1,18 @@
 package lotto.view.sin;
 
 
-import base.view.InputView;
-import lotto.model.entity.money.IMoneyInputDto;
+import base.view.View;
+import lotto.model.money.IMoneyInput;
 
-public class MoneyInputView extends InputView<IMoneyInputDto> {
+public class MoneyInputView extends View<IMoneyInput> {
     @Override
     public void header() {
+        super.header();
         System.out.println("구입금액을 입력해 주세요.");
+    }
+
+    @Override
+    public void render(IMoneyInput iMoneyInput) {
+
     }
 }
--- src/main/java/lotto/view/sout/PrizeStatisticsOutputView.java
+++ src/main/java/lotto/view/sout/PrizeStatisticsOutputView.java
@@ -1,20 +1,28 @@
 package lotto.view.sout;
 
-import base.view.OutputView;
-import lotto.model.entity.lotto.prize.stats.StatsOutputDto;
+import base.view.View;
+import lotto.model.stats.IStatsOutput;
+import lotto.model.stats.StatsOutputDto;
 
-public class PrizeStatisticsOutputView extends OutputView<StatsOutputDto> {
+public class PrizeStatisticsOutputView extends View<IStatsOutput> {
 
-    public void render(StatsOutputDto prizeStatsOutputDto) {
+    public PrizeStatisticsOutputView() {
+        header();
+    }
+
+    public void render(IStatsOutput dto) {
+        var prizeStatsOutputDto = new StatsOutputDto(dto.prizeStats());
         System.out.println(prizeStatsOutputDto);
         printProfitRate(prizeStatsOutputDto);
     }
 
     @Override
-    protected void header() {
+    public void header() {
+        super.header();
         System.out.println("당첨 통계");
         System.out.println("---");
     }
+
 
     private void printProfitRate(StatsOutputDto prizeStatsOutputDto) {
         System.out.println("총 수익률은 " + prizeStatsOutputDto.profitRate() + "%입니다.");
